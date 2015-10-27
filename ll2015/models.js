deaef92c7ec4e15f84cd00a793be213c068e9300
Lotus Lien <lotuslien@users.noreply.github.com>
2015-10-26 23:11:04 -0400

    Create models.js

---
+++ ll2015/models.js
@@ -0,0 +1,32 @@
+
+MX.Tableaux.Models = MX.Tableau.extend({
+
+	init: function(opt){
+
+		this.opt = opt = defaults(opt, {
+			width: 100,
+			height: 100,
+			depth: 100,
+			x: 0,
+			y: 0,
+			z: 0,
+			rotationY: 0,
+			rotationX: 0,
+			scale: 1,
+			spacingY: 0,
+		})
+
+		for (var i = 1; i <= 9; i++) {
+			girl = new MX.Image({
+				src: "http://okfocus.s3.amazonaws.com/kenzo/img/" + i + ".png",
+				x: (100 + (9-i) * 40) * pow(-1, i),
+				y: (i-1) * opt.spacingY + opt.y,
+				z: 400 * i + opt.z,
+				scale: 0.5,
+				rotationY: PI/6 * pow(-1, i+1)
+			})
+			scene.add(girl)
+		}
+	}
+
+});
