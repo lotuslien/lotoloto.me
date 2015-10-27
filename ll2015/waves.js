3d3779c418e10b4488ab19d28104b1ff898077cc
Lotus Lien <lotuslien@users.noreply.github.com>
2015-10-26 23:11:42 -0400

    Create waves.js

---
+++ ll2015/waves.js
@@ -0,0 +1,34 @@
+
+MX.Tableaux.Waves = MX.Tableau.extend({
+
+	init: function(opt){
+		
+		opt = defaults(opt, {
+			x: 0,
+			y: 0,
+			z: 0,
+			gap: 0,
+		})
+
+		wave = new MX.Image({
+			src: "http://okfocus.s3.amazonaws.com/kenzo/img/waves_blue.png",
+			x: opt.x - 200 - opt.gap/2,
+			y: opt.y + 200,
+			z: opt.z + 60,
+			scale: 1.3,
+		})
+		wave.rotationY = PI/4
+		scene.add(wave)
+
+		wave = new MX.Image({
+			src: "http://okfocus.s3.amazonaws.com/kenzo/img/waves_blue.png",
+			x: opt.x + 200 + opt.gap/2,
+			y: opt.y + 400,
+			z: opt.z + 100,
+			scale: 1.3,
+		})
+		wave.rotationY = Math.PI - PI/4
+		scene.add(wave)
+	}
+
+});
