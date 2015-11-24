adfd0497ff4a2abac1b51e8fb938f4329dcbc8f3
Lotus Lien <lotuslien@users.noreply.github.com>
2015-11-24 10:50:37 -0500

    Update waves.js

--- ll2015/waves.js
+++ ll2015/waves.js
@@ -30,15 +30,7 @@ MX.Tableaux.Waves = MX.Tableau.extend({
 		wave.rotationY = Math.PI - PI/4
 		scene.add(wave)
 		
-		wave = new MX.Image({
-			src: "https://cloud.githubusercontent.com/assets/8115609/11075190/f7261a42-87c1-11e5-99ba-094a7b86714c.gif",
-			x: opt.x + 200 + opt.gap/2,
-			y: opt.y + 600,
-			z: opt.z + 140,
-			scale: 1.3,
-		})
-		wave.rotationY = Math.PI - PI/4
-		scene.add(wave)
+	
 	}
 
 });
