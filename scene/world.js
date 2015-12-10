37153f4f4b330b31ca7c1c6d2945ea180ac00885
Lotus Lien <lotuslien@users.noreply.github.com>
2015-12-10 17:39:44 -0500

    Create world.js

---
+++ scene/world.js
@@ -0,0 +1,236 @@
+var World = function() {
+  var camera, cubeCamera, cubeMap, scene, renderer, controls, stats;
+  var fov = 70,
+    isUserInteracting = false,
+    onMouseDownMouseX = 0,
+    onMouseDownMouseY = 0,
+    lon = 0,
+    onMouseDownLon = 0,
+    lat = 0,
+    onMouseDownLat = 0,
+    phi = 0,
+    theta = 0;
+  var clock = new THREE.Clock();
+  var envMesh;
+  var tempLight;
+  var lights = [];
+  var frameCounter = 0;
+  var nodes = [];
+  var numLights = 3;
+  var sphereRadius = 30;
+  var lightRange = 0;
+  var movementSpeed = 77;
+  var lookSpeed = .11;
+  var lightIntensity = 33.0;
+  var lightDistance = 200;
+  var numNodes;
+  var tempNode;
+  var time;
+  var self;
+
+
+  function init() {
+    self = this;
+
+    camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, 1, 1000);
+
+    scene = new THREE.Scene();
+
+    //SKY BOX
+
+    var urls = [
+        'https://cloud.githubusercontent.com/assets/8115609/11728632/eb1922d6-9f58-11e5-8577-8da39909c278.jpg',
+        'https://cloud.githubusercontent.com/assets/8115609/11728632/eb1922d6-9f58-11e5-8577-8da39909c278.jpg',
+        'https://cloud.githubusercontent.com/assets/8115609/11728632/eb1922d6-9f58-11e5-8577-8da39909c278.jpg',
+        'https://cloud.githubusercontent.com/assets/8115609/11728632/eb1922d6-9f58-11e5-8577-8da39909c278.jpg',
+        'https://cloud.githubusercontent.com/assets/8115609/11728632/eb1922d6-9f58-11e5-8577-8da39909c278.jpg',
+        'https://cloud.githubusercontent.com/assets/8115609/11728632/eb1922d6-9f58-11e5-8577-8da39909c278.jpg'
+    ];
+
+    cubemap = THREE.ImageUtils.
+    loadTextureCube(urls);
+
+
+
+    var envMaterial = new THREE
+      .MeshLambertMaterial({
+      color: 0xffffff,
+      envMap: cubemap
+    });
+
+
+
+
+    //ENVIRONMENT MAP
+    envMesh = new THREE.Mesh(new THREE.SphereGeometry(500, 60, 40), envMaterial);
+    envMesh.scale.x = -1;
+    scene.add(envMesh);
+
+    renderer = new THREE.WebGLRenderer({
+      antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor(0x000000);
+    cubeCamera = new THREE.CubeCamera(1, 1000, 256);
+    cubeCamera.renderTarget.minFilter = THREE.LinearMipMapLinearFilter;
+    scene.add(cubeCamera);
+
+
+
+    //LIGHTS 
+    scene.add(new THREE.AmbientLight(0xffffff));
+
+
+
+    //CONTROLS
+    controls = new THREE.FirstPersonControls(camera);
+
+    controls.movementSpeed = movementSpeed;
+    controls.lookSpeed = lookSpeed;
+    controls.freeze;
+
+    controls.lon = -90;
+
+
+
+    createNodes();
+    addLights();
+
+    window.addEventListener('resize', onWindowResized, false);
+    document.addEventListener('mousewheel', onDocumentMouseWheel, false);
+    document.addEventListener('DOMMouseScroll', onDocumentMouseWheel, false);
+
+    onWindowResized(null);
+
+    this.renderer = renderer;
+
+  }
+
+
+  function addLights() {
+    for (var i = 0; i < numLights; i++) {
+      var nodeIndex = Math.floor(Math.random() * nodes.length);
+      var node = nodes[nodeIndex];
+      var randColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      var light = new THREE.PointLight(randColor, lightIntensity, lightDistance)
+      var lightPosition = new THREE.Vector3();
+      lightPosition.x = node.originalPosition.x;
+      lightPosition.y = node.originalPosition.y;
+      lightPosition.z = node.originalPosition.z;
+      lights.push({
+        originalPosition: lightPosition,
+        sceneLight: light
+      });
+      scene.add(light);
+    }
+  }
+
+  function onWindowResized(event) {
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.projectionMatrix.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
+    controls.handleResize();
+  }
+
+  function onDocumentMouseWheel(event) {
+
+    // WebKit
+
+    if (event.wheelDeltaY) {
+
+      fov -= event.wheelDeltaY * 0.005;
+
+      // Opera / Explorer 9
+
+    } else if (event.wheelDelta) {
+
+      fov -= event.wheelDelta * 0.005;
+
+      // Firefox
+
+    } else if (event.detail) {
+
+      fov += event.detail * 1.0;
+
+    }
+
+    camera.projectionMatrix.makePerspective(fov, window.innerWidth / window.innerHeight, 1, 1100);
+
+  }
+
+  function animate() {
+
+    requestAnimationFrame(self.animate);
+    render();
+
+  }
+
+  function render() {
+    time = Date.now() * .0004;
+    var delta = clock.getDelta();
+
+    for (var i = 0; i < numLights; i++) {
+      tempLight = lights[i];
+      tempLight.sceneLight.position.x = Math.sin(time * .5) * 111 + tempLight.originalPosition.x;
+      tempLight.sceneLight.position.y = Math.cos(time * .5) * 111 + tempLight.originalPosition.y;
+      tempLight.sceneLight.position.z = Math.cos(time * .5) * 111 + tempLight.originalPosition.z;
+
+    }
+
+    for (var i = 0; i < numNodes; i++) {
+      tempNode = nodes[i];
+      if (i % 2 === 0) {
+        tempNode.sphere.position.x = Math.sin(time * .2) * 3 + tempNode.originalPosition.x;
+        tempNode.sphere.position.y = Math.cos(time * .5) * 5 + tempNode.originalPosition.y;
+        tempNode.sphere.position.z = Math.cos(time * .3) * 2 + tempNode.originalPosition.z;
+      } else {
+        tempNode.sphere.position.x = Math.sin(time * .3) * 1 + tempNode.originalPosition.x;
+        tempNode.sphere.position.y = Math.cos(time * .4) * 8 + tempNode.originalPosition.y;
+        tempNode.sphere.position.z = Math.cos(time * .2) * 1 + tempNode.originalPosition.z;
+      }
+
+    }
+
+    cubeCamera.updateCubeMap(renderer, scene);
+
+    controls.update(clock.getDelta());
+    renderer.render(scene, camera);
+
+  }
+
+  function createNodes() {
+    var cubeTarget = cubeCamera.renderTarget;
+    var shinyMaterial = new THREE.MeshPhongMaterial({
+      color: 0xffffff,
+      ambient: 0xffffff,
+      envMap: cubeTarget
+    });
+    var spacing = sphereRadius * 4;
+    var sphere;
+    var begin = -300;
+    var end = 300
+
+    for (var x = begin; x < end; x += spacing) {
+      for (var y = begin; y < end; y += spacing) {
+        for (var z = begin; z < end; z += spacing) {
+          sphere = new THREE.Mesh(new THREE.SphereGeometry(sphereRadius, 30, 15), shinyMaterial);
+          var position = new THREE.Vector3(x + Math.random() * 20, y + Math.random() * 20, z + Math.random() * 20);
+          sphere.position.copy(position);
+          nodes.push({
+            originalPosition: position,
+            sphere: sphere
+          });
+          scene.add(sphere);
+        }
+
+      }
+
+    }
+    numNodes = nodes.length;
+  }
+
+  this.init = init;
+  this.animate = animate;
+
+  return this;
+}
