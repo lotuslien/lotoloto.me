9daae63dcb8be95629f92ff10c2e4dccb1f3196a
Lotus Lien <lotuslien@users.noreply.github.com>
2015-12-10 21:30:51 -0500

    Update game2.js

--- ping/game2.js
+++ ping/game2.js
@@ -114,7 +114,10 @@ function createScene()
 	var pillarMaterial =
 	  new THREE.MeshLambertMaterial(
 		{
-		  color: 0x534d0d
+		  color: 0x534d0d,
+		  specular: 0x009900, 
+		  shininess: 30, 
+		  shading: THREE.FlatShading
 		});
 	// create the ground's material
 	var groundMaterial =
