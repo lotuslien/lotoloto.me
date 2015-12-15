369c599d54678a171a0720df4ca8211473ece3d5
Lotus Lien <lotuslien@users.noreply.github.com>
2015-12-15 01:54:08 -0500

    Update fish_cutouts.js

--- ll2015/fish_cutouts.js
+++ ll2015/fish_cutouts.js
@@ -23,21 +23,21 @@ MX.Tableaux.FishCutouts = MX.Tableau.extend({
 			patternSrc: "http://www.transparenttextures.com/patterns/concrete-wall.png"
 		})
 	
-		var fish_base = 'http://okfocus.s3.amazonaws.com/kenso/underwater/'
+		var fish_base = 'http://www.transparenttextures.com/patterns/'
 		var fish_svgs = [
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png',
-			'bwrepeat2.png'
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png',
+			'concrete-wall.png'
 		]
 		opt.xRange /= 2
 		opt.zRange /= 2
